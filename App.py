3606ea4d88e6b934bf10e8e5275c6cc2e8965375
Fabio Dani <200219294+FabioDani2295@users.noreply.github.com>
2025-02-21 13:28:44 +0100

    Primo

---
+++ App.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("La mia prima app con Streamlit!")
+st.write("Benvenuto nella mia applicazione web interattiva.")
+
+# Upload di file
+uploaded_file = st.file_uploader("Carica un file CSV", type="csv")
+if uploaded_file:
+    import pandas as pd
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head())
+
