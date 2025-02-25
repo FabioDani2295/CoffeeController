767860f83615c487f1c091f1ce2a704e529ee23b
Fabio Dani <200219294+FabioDani2295@users.noreply.github.com>
2025-02-25 11:03:09 +0100

    dashboard caffe 1 con immagine

--- App.py
+++ App.py
@@ -4,14 +4,16 @@ import Data
 
 st.set_page_config(page_title="📊 Environmental Dashboard", page_icon="🌍", layout="wide")
 
-# **Mostrare l'Immagine in Alto**
-st.image("ImageData.jpg", caption="📸 Analisi Ambientale", use_container_width=False)
+
 
 # **Caricare i dati**
 df = Data.load_data()
 
 # **Mostrare la dashboard con grafici**
 Charts.display_charts(df)
-
 st.markdown("---")
+# **Mostrare immagine
+st.image("ImageData.jpg", caption="📸 Analisi Ambientale", use_container_width=False)
+
+
 
