74b2e5f13cdc76cba61375598e8b8aa01e69536a
Fabio Dani <200219294+FabioDani2295@users.noreply.github.com>
2025-02-25 11:08:13 +0100

    referesh ogni 10 sec

--- App.py
+++ App.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import Charts
 import Data
+from streamlit_autorefresh import st_autorefresh  # Import per l'aggiornamento automatico
 
 st.set_page_config(page_title="📊 Environmental Dashboard", page_icon="🌍", layout="wide")
 
-
+st_autorefresh(interval=10 * 1000, key="data_refresh")  # 10 secondi (10 * 1000 ms)
 
 # **Caricare i dati**
 df = Data.load_data()
